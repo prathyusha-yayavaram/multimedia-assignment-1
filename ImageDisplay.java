38427a896564675d1552282d013c8a2df3d1e730
Prathyusha Yayavaram <yayavara@usc.edu>
2024-02-09 19:57:10 -0800

    Fix fps, cleanup and add comments

--- ImageDisplay.java
+++ ImageDisplay.java
@@ -1,6 +1,3 @@
-
-import sun.rmi.server.Activation$ActivationSystemImpl_Stub;
-
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
@@ -12,7 +9,9 @@ public class ImageDisplay {
 	JFrame frame;
 	JLabel lbIm1;
 	BufferedImage imgOne;
-	BufferedImage imgFinal;
+	BufferedImage imgPerFrame;
+
+	short avgRGB[][][];
 
 	// Modify the height and width values here to read and display an image with
   	// different dimensions. 
@@ -42,13 +41,11 @@ public class ImageDisplay {
 			{
 				for(int x = 0; x < width; x++)
 				{
-					byte a = 0;
 					byte r = bytes[ind];
 					byte g = bytes[ind+height*width];
 					byte b = bytes[ind+height*width*2]; 
 
 					int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-					//int pix = ((a << 24) + (r << 16) + (g << 8) + b);
 					img.setRGB(x,y,pix);
 					ind++;
 				}
@@ -66,12 +63,18 @@ public class ImageDisplay {
 
 	public void showIms(String[] args) throws InterruptedException {
 
-		// Read in the specified image
+		// Original Image
 		imgOne = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		imgFinal = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		readImageRGB(width, height, args[0], imgOne);
-		int cnt = 5;
+		// Transformed image per frame
+		imgPerFrame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+		readImageRGB(width, height, args[0], imgOne);
+
+		// Precompute 3x3 avg RGB values
+		avgRGB = new short[width][height][3];
+		calculateAverages();
+
+		// Initialize JFrame to display images
 		frame = new JFrame();
 		GridBagLayout gLayout = new GridBagLayout();
 		frame.getContentPane().setLayout(gLayout);
@@ -85,7 +88,7 @@ public class ImageDisplay {
 		c.gridx = 0;
 		c.gridy = 1;
 
-		lbIm1.setIcon(new ImageIcon(imgFinal));
+		lbIm1.setIcon(new ImageIcon(imgPerFrame));
 		frame.getContentPane().add(lbIm1, c);
 		frame.revalidate();
 		frame.repaint();
@@ -93,45 +96,74 @@ public class ImageDisplay {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		// Get input values from args
 		double zoom = Double.parseDouble(args[1]) - 1;
 		double rotationAngle = Double.parseDouble(args[2]);
+		int fps = Integer.parseInt(args[3]);
+
 		double curZoom = 1;
 		double curRotationAngle = 0;
-		int fps = Integer.parseInt(args[3]);
+
+		//Calculate zoom and rotation increment factor based on fps
 		double zoomIncrementFactor = zoom/fps;
 		double rotationIncrementFactor = rotationAngle/fps;
-		long startTime = System.currentTimeMillis();
-		while (curZoom > 0 && curZoom < 50) {
 
+		long startTime = System.currentTimeMillis();
+		while (curZoom > 0) {
 			curZoom += zoomIncrementFactor;
-			// Check if curZoom is very close to zero and consider it as zero
 			if (curZoom < 0) {
 				curZoom = 0;
 			}
 			curRotationAngle += rotationIncrementFactor;
 			double angleInRadians = Math.toRadians(curRotationAngle);
-			performImageAction(curZoom, angleInRadians, imgOne);
 
-			// Update the image in the existing JLabel
-			lbIm1.setIcon(new ImageIcon(imgFinal));
-			frame.getContentPane().add(lbIm1, c);
-			frame.revalidate();
+			//Set transformed image
+			performImageTransformation(curZoom, angleInRadians);
+
+			//Repaint the image in JFrame
 			frame.repaint();
 			frame.pack();
+
+			long endTime = System.currentTimeMillis();
+			long elapsedTime = endTime - startTime;
+
+			// Calculate sleep time to achieve given fps. 4 ms correction to fix time taken to calculate the time
+			Thread.sleep(Math.max(0,(1000/fps) - elapsedTime - 4));
+
 			frame.setVisible(true);
 
-			// Pause for 2 seconds
-			Thread.sleep((long)Math.max(0,(1000/fps)-33.33));
+			startTime = System.currentTimeMillis();
 		}
-		long endTime = System.currentTimeMillis();
-		long elapsedTime = endTime - startTime;
-		System.out.println("Time taken: " + elapsedTime + " milliseconds");
-
-// Ensure the program exits when the window is closed
-
 	}
 
-	private void performImageAction(double zoom, double angleInRadians, BufferedImage img) {
+	//Precompute 3x3 avg RGB values for each pixel in the original image
+	private void calculateAverages() {
+		int filterFactor = 1;
+		for (int xOldInt = 0; xOldInt < width; xOldInt++) {
+			for (int yOldInt = 0; yOldInt < height; yOldInt++) {
+				int red = 0, blue = 0, green = 0;
+				int cnt = 0;
+
+				for (int i = xOldInt - filterFactor; i <= xOldInt + filterFactor; i++) {
+					for (int j = yOldInt - filterFactor; j <= yOldInt + filterFactor; j++) {
+						if (i >= 0 && i < width && j >= 0 && j < height) {
+							cnt++;
+							int rgb = imgOne.getRGB(i, j);
+							red += (rgb >> 16) & 0xFF;
+							green += (rgb >> 8) & 0xFF;
+							blue += rgb & 0xFF;
+						}
+					}
+				}
+				avgRGB[xOldInt][yOldInt][0] = (short) (red/cnt);
+				avgRGB[xOldInt][yOldInt][1] = (short) (green/cnt);
+				avgRGB[xOldInt][yOldInt][2] = (short) (blue/cnt);
+			}
+		}
+	}
+
+	// Perform the image transformation using inverse mapping for given zoom and rotation step values
+	private void performImageTransformation(double zoom, double angleInRadians) {
 		double cosValue = Math.cos(angleInRadians);
 		double sinValue = Math.sin(angleInRadians);
 		double invZoom = 1 / zoom;
@@ -142,43 +174,31 @@ public class ImageDisplay {
 			for (int x = 0; x < width; x++) {
 				int xt = x - widthDiv2;
 				int yt = y - heightDiv2;
+
+				//Inverse transformation and rounding to map new pixel to old pixel
 				double xOld = invZoom * cosValue * xt + invZoom * sinValue * yt + widthDiv2;
 				double yOld = -invZoom * sinValue * xt + invZoom * cosValue * yt + heightDiv2;
-				int xOldInt = (int) Math.floor(xOld);
-				int yOldInt = (int) Math.floor(yOld);
+				int xOldInt = (int) Math.round(xOld);
+				int yOldInt = (int) Math.round(yOld);
 
-				if (zoom != 0 && xOldInt >= 0 && xOldInt < 512 && yOldInt >= 0 && yOldInt < 512) {
-					setRGBValueFromOld(img, y, x, xOldInt, yOldInt, zoom);
+				if (zoom != 0 && xOldInt >= 0 && xOldInt < width && yOldInt >= 0 && yOldInt < height) {
+					setRGBValueFromOld(y, x, xOldInt, yOldInt, zoom);
 				} else {
-					imgFinal.setRGB(x, y, 0);
+					imgPerFrame.setRGB(x, y, 0);
 				}
 			}
 		}
 	}
 
-	private void setRGBValueFromOld(BufferedImage img, int y, int x, int xOldInt, int yOldInt, double zoom) {
+	//Set RGB values based on zoom factor. Apply filter only when zooming out
+	private void setRGBValueFromOld(int y, int x, int xOldInt, int yOldInt, double zoom) {
 		if (zoom > 1) {
-			int rgb = img.getRGB(xOldInt, yOldInt);
-			imgFinal.setRGB(x, y, rgb);
+			int rgb = imgOne.getRGB(xOldInt, yOldInt);
+			imgPerFrame.setRGB(x, y, rgb);
 		} else {
-			int filterFactor = Math.max((int) (1 / zoom), 1);
-			int red = 0, blue = 0, green = 0;
-			int cnt = 0;
-
-			for (int i = xOldInt - filterFactor; i <= xOldInt + filterFactor; i++) {
-				for (int j = yOldInt - filterFactor; j <= yOldInt + filterFactor; j++) {
-					if (i >= 0 && i < 512 && j >= 0 && j < 512) {
-						cnt++;
-						int rgb = img.getRGB(i, j);
-						red += (rgb >> 16) & 0xFF;
-						green += (rgb >> 8) & 0xFF;
-						blue += rgb & 0xFF;
-					}
-				}
-			}
-
-			int avgRGB = 0xff000000 | ((red / cnt) & 0xFF) << 16 | ((green / cnt) & 0xFF) << 8 | (blue / cnt) & 0xFF;
-			imgFinal.setRGB(x, y, avgRGB);
+			int red = avgRGB[xOldInt][yOldInt][0], blue = avgRGB[xOldInt][yOldInt][1], green = avgRGB[xOldInt][yOldInt][2];
+			int avgRGB = 0xff000000 | ((red) & 0xFF) << 16 | ((green) & 0xFF) << 8 | (blue) & 0xFF;
+			imgPerFrame.setRGB(x, y, avgRGB);
 		}
 	}
 
